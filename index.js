63de69e6e28f6bcb064fca81cd5cb81ccb0f2336
pvkhanh <pvkhanh@vnpt.vn>
2024-05-24 13:48:35 +0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,78 @@
 function toggleSidebar() {
-  const sidebar = document.getElementById("mySidebar");
+  const sidebar = document.getElementById('mySidebar');
   const body = document.body;
-  const icon = document.querySelector(".toggle-icon");
+  const icon = document.querySelector('.toggle-icon');
 
-  if (sidebar.style.display === "none") {
-    sidebar.style.display = "block";
-    body.classList.add("body-no-scroll");
-    icon.classList.remove("bi-list");
-    icon.classList.add("bi-x");
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block';
+    body.classList.add('body-no-scroll');
+    icon.classList.remove('bi-list');
+    icon.classList.add('bi-x');
   } else {
-    sidebar.style.display = "none";
-    body.classList.remove("body-no-scroll");
-    icon.classList.remove("bi-x");
-    icon.classList.add("bi-list");
+    sidebar.style.display = 'none';
+    body.classList.remove('body-no-scroll');
+    icon.classList.remove('bi-x');
+    icon.classList.add('bi-list');
   }
 }
+
+// toggle submenu
+function toggleSubmenu(event) {
+  event.stopPropagation(); // Prevent the click event from propagating to the document body
+  var subMenu = document.querySelector('.sub-menu');
+  subMenu.classList.toggle('sub-menu-open'); // Toggle the class
+}
+
+document.body.addEventListener('click', function (event) {
+  var seikoLink = document.querySelector('.seiko');
+  var subMenu = document.querySelector('.sub-menu');
+  if (!event.target.closest('.seiko')) {
+    // Check if the clicked element or its ancestors contain the .seiko class
+    subMenu.classList.remove('sub-menu-open'); // Remove the class
+  }
+});
+
+// toggle search box
+function toggleSearchBox() {
+  const body = document.body;
+  const searchComponent = document.querySelector('.search-component');
+  const boxHeader = document.querySelector('.box-header');
+
+  if (searchComponent.style.display === 'none') {
+    searchComponent.style.display = 'block';
+    boxHeader.style.display = 'none';
+    body.classList.add('body-no-scroll');
+    // icon.classList.remove('bi-list');
+    // icon.classList.add('bi-x');
+  } else {
+    searchComponent.style.display = 'none';
+    boxHeader.style.display = 'flex';
+    body.classList.remove('body-no-scroll');
+    // icon.classList.remove('bi-x');
+    // icon.classList.add('bi-list');
+  }
+}
+
+// prevent close filter on seiko page
+document
+  .querySelectorAll('.dropdown-menu .item[data-stayopen="true"]')
+  .forEach(function (item) {
+    item.addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
+  });
+
+// counter in cart page
+document.addEventListener('DOMContentLoaded', function () {
+  const counter = document.getElementById('counter');
+  const incrementBtn = document.getElementById('increment');
+  const decrementBtn = document.getElementById('decrement');
+
+  incrementBtn.addEventListener('click', function () {
+    counter.textContent = parseInt(counter.textContent) + 1;
+  });
+
+  decrementBtn.addEventListener('click', function () {
+    counter.textContent = parseInt(counter.textContent) - 1;
+  });
+});
